10d9fd49a3be0a0f7b3441ba3f819b67a8d48af2
amateurdullahan <zabrak12@gmail.com>
2020-04-28 23:42:38 +0000

    fixed extra whitespace

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     for letter in str:
         x = ord(letter)
-        if  x >= 97 and x <= 122:
+        if x >= 97 and x <= 122:
             x = x - 32
         print("{:c}".format(x), end='')
     print()
