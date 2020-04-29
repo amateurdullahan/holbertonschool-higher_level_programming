5bb77f45c92067fc4f170448f8049aa7a5dd051e
amateurdullahan <zabrak12@gmail.com>
2020-04-29 15:01:07 +0000

    fixed line spacing

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,9 +4,9 @@ number = random.randint(-10000, 10000)
 ld = number % 10
 if ld > 5:
     print("last digit of {:d} is {:d} \
-    and is greater than 5".format(number, ld))
+and is greater than 5".format(number, ld))
 elif ld == 0:
     print("last digit of {:d} is {:d} and is 0".format(number, ld))
 else:
     print("last digit of {:d} is {:d} \
-    and is less than 6 and not 0".format(number, ld))
+and is less than 6 and not 0".format(number, ld))
