38133cabb4c2ed904ae355f8b7f71b2aa63ababd
amateurdullahan <zabrak12@gmail.com>
2020-04-30 21:31:40 +0000

    added code

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
+    list = dir(hidden_4)
+    for x in range(len(list)):
+        if (list[x][0] != "_" and list[x][1] != "_"):
+            print("{:s}".format(list[x]))
