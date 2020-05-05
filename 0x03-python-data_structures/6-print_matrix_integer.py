ea5787f4f59bf28bc9581504a0cb3f3de94a1bff
amateurdullahan <zabrak12@gmail.com>
2020-05-05 22:09:09 +0000

    updated print statement formats to meet requirements

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,7 +5,7 @@ def print_matrix_integer(matrix=[[]]):
     for a in range(len(matrix)):
         for b in range(len(matrix[a])):
             if b < len(matrix[a]) - 1:
-                print(matrix[a][b], end=' ')
+                print("{:d}".format(matrix[a][b]), end=' ')
             else:
-                print(matrix[a][b], end='')
+                print("{:d}".format(matrix[a][b]), end='')
         print("")
