8d1076a84c508903abde6a4d1457d39077254dc1
amateurdullahan <zabrak12@gmail.com>
2020-05-20 19:57:06 +0000

    fixed UnboundLocalError

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    a = 0
+    if x == 0:
+        return a
     for a in range(x):
         try:
             print("{}".format(my_list[a]), end="")
