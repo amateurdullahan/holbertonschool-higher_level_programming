7a52cc2fcca5c9fe6fe6677b5b72b2630d01f5c3
amateurdullahan <zabrak12@gmail.com>
2020-05-20 01:27:47 +0000

    pep8

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-"""Documentation for Square Class"""
+"""Documentation for Square Class."""
 
 
-class Square():
-    """Empty Square Class
-
-       Attributes:
-          None
-    """
+class Square:
+    """Empty Square Class, no attributes."""
     pass
