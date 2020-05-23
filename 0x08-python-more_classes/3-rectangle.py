00d8d2382ae8be594b6f33a04ff21deec9273509
amateurdullahan <zabrak12@gmail.com>
2020-05-23 18:28:28 +0000

    add 3-rectangle

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""Real Rectangle Class"""
+
+
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__width = width
+            self.__height= height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.height
+    
+    
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return (self.__height * self.__width)
+    
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        else:
+            return ((self.__height * 2) + (self.__width * 2))
+
+    def rect_print(self):
+        if self.__height == 0 or self.__width == 0:
+            print()
+            return
+        else:
+            for a in range(self.__height):
+                for b in range(self.__width):
+                    print('#', end='')
+                print()
