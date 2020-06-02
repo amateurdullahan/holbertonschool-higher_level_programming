fe42848e6876ae336fb77450baaf22719608d9f6
amateurdullahan <zabrak12@gmail.com>
2020-06-02 23:26:03 +0000

    task 11 and fixing mispellings in 10

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -10,3 +10,7 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
+
+    def __str__(self):
+        """print string"""
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
