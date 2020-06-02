9a255d6baef253deeb82ffe2d058dfb914e2a6af
amateurdullahan <zabrak12@gmail.com>
2020-06-02 22:44:59 +0000

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""mostly geometry class"""
+
+
+class BaseGeometry:
+    """mostly empty class"""
+    def area(self):
+        """raise exception"""
+        raise Exception("area() is not implemented")
