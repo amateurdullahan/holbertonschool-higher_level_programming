7c6f26678714a466d24f28b07084c621428cb263
amateurdullahan <zabrak12@gmail.com>
2020-06-02 23:34:55 +0000

    pep8 fix

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,7 +7,7 @@ class MyInt(int):
     def __eq__(self, other):
         """oppo"""
         return super().__ne__(other)
-    
+
     def __ne__(self, other):
-       """site"""
-       return super().__eq__(other)
+        """site"""
+        return super().__eq__(other)
