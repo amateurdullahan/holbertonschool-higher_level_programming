e99822855b5d0b9f53062c1814235ab9dba252a5
amateurdullahan <zabrak12@gmail.com>
2020-06-03 20:26:32 +0000

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""contains Student, to_json"""
+
+
+class Student:
+    """class is in session"""
+    def __init__(self, first_name, last_name, age):
+        """init"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """dict"""
+        return self.__dict__
