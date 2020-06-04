41ec182ab815014c5f077ab2ad6c88c028787abd
amateurdullahan <zabrak12@gmail.com>
2020-06-04 00:41:29 +0000

    bbb

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -13,7 +13,7 @@ class Student:
     def to_json(self, attrs=None):
         """dict"""
         lson = {}
-        if attrs = None:
+        if attrs = None or type(attrs) != list:
             return self.__dict__
         else:
             for item in self.__dict__:
