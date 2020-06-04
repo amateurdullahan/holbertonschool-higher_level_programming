babed6b88f24c2980e006e3fa1f0eaf4f5cc7707
amateurdullahan <zabrak12@gmail.com>
2020-06-04 00:44:41 +0000

    task 13

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""contains Student, to_json"""
+
+
+class Student:
+    """class is in session"""
+    def __init__(self, first_name, last_name, age):
+        """init"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """dict"""
+        lson = {}
+        if attrs is None or type(attrs) != list:
+            return self.__dict__
+        else:
+            for item in self.__dict__:
+                if item in attrs:
+                    lson[item] = self.__dict__[item]
+            return lson
+
+    def reload_from_json(self, json):
+        """replaces all attributes from Student"""
+        for items in json.key():
+            self.__dict__[items] = json[items]
