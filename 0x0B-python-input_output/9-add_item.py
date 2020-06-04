3e0c1d9fadca71261ad27d23bf48416b38a924ff
amateurdullahan <zabrak12@gmail.com>
2020-06-04 00:24:01 +0000

    changed import

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """9-add_item"""
-import sys
+from sys import argv
+
+
+
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
@@ -10,7 +13,7 @@ try:
 except:
     json_list = []
 
-for c in range(1, len(sys.argv)):
+for c in range(1, len(argv)):
     json_list.append(argv[c])
 
 save_to_json_file(json_list, "add_item.json")
