6c627fd63ac31a2185e9e0411aaf586efc03a78f
amateurdullahan <zabrak12@gmail.com>
2020-06-04 00:26:02 +0000

    idk messing with variables

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -9,11 +9,12 @@ load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 
 try:
-    json_list = load_from_json_file("add_item.json")
+    filename = "add_item.json"
+    json_list = load_from_json_file(filename)
 except:
     json_list = []
 
 for c in range(1, len(argv)):
     json_list.append(argv[c])
 
-save_to_json_file(json_list, "add_item.json")
+save_to_json_file(json_list, filename)
