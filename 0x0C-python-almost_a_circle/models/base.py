79dca5df3b2d61156b699cbaddabb9cf5f4bb3f7
amateurdullahan <zabrak12@gmail.com>
2020-06-08 22:15:30 +0000

    add task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Base file"""
+import json
 
 
 class Base:
@@ -13,3 +14,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """return dump"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
