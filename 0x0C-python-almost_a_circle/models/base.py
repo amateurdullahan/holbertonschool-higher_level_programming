ff05adb043b1da956387daac58bd0d9ca6f254f5
amateurdullahan <zabrak12@gmail.com>
2020-06-08 22:26:21 +0000

    add task 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -15,8 +15,23 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """return dump"""
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """write json representation to file"""
+        nlist = []
+        for obj in list_objs:
+            nlist.append(obj.to_dictionary())
+        nstr = (cls.to_json_string(nlist))
+        fname = "{}.json".format(cls.__name__)
+        with open(fname, 'w') as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                f.write(nstr)
