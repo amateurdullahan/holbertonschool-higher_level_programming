21672c4be88cb817901b4ef59c79817d83d95eb5
amateurdullahan <zabrak12@gmail.com>
2020-06-08 20:56:42 +0000

    task 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square file"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size, x=0, y=0, id=None):
+        """init square"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """overwritten str"""
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(
+            self.id, self.__x, self.__y, self.__size)
