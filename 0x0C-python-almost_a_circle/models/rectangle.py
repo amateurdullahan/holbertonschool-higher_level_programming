e59c5b2379fb502770ab4e456b2353d6a086559f
amateurdullahan <zabrak12@gmail.com>
2020-06-08 00:54:05 +0000

    bunch of updates

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 """Rectangle file"""
-Base = __import__('base').Base
+from models.base import Base
 
 
 class Rectangle(Base):
     """Rectangle Class that inherits from Base"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """Rectangle init"""
-        self.id = super().__init__(id=None)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.integer_validator("x", x)
+        self.integer_validator("y", y)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
 
     @property
     def width(self):
@@ -21,7 +25,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """set height"""
-        self.__width = value
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
 
     @property
     def height(self):
@@ -31,7 +38,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """set height"""
-        self.__height = value
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = value
 
     @property
     def x(self):
@@ -39,7 +49,10 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        self.__x = value
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = value
 
     @property
     def y(self):
@@ -47,4 +60,15 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        self.__y = value
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = value
+
+    def integer_validator(self, name, value):
+        """validate integer"""
+        if not isinstance(value, int):
+            raise TypeError("{:s} must be an integer".format(name))
+
+    def area(self):
+        return self.__width * self.__height
