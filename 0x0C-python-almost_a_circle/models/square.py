0060f8e526c2d7d033733c0936ac92a230e2801d
amateurdullahan <zabrak12@gmail.com>
2020-06-08 21:42:51 +0000

    add task 12, fixed setter and init and str

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,24 +6,38 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """init square"""
+        self.size = size
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """overwritten str"""
         return "[Square] ({:d}) {:d}/{:d} - {:d}".format(
-            self.id, self.x, self.y, self.width)
+            self.id, self.x, self.y, self.size)
 
     @property
     def size(self):
-        """get size, which is width"""
-        return self.width
+        """get size"""
+        return self.__size
 
     @size.setter
-    def size(self, size):
+    def size(self, value):
         """set width and height to same value"""
-        if type(size) is not int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
-        if size <= 0:
+        if value <= 0:
             raise ValueError("width must be >= 0")
-        self.__width = size
-        self.__height = size
+        self.__width = value
+        self.__height = value
+        self.__size = value
+
+    def update(self, *args, **kwargs):
+        """update square"""
+        udict = {0: self.id, 1: self.size, 2: self.x, 3: self.y}
+        if args:
+            for c, update in enumerate(args):
+                udict[c] = update
+                self.id, self.size, self.x, self.y = (
+                udict[0], udict[1], udict[2], udict[3])
+        else:
+            for key, update in kwargs.items():
+                setattr(self, key, update)
