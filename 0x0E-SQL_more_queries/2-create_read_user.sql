ddf284e63851e831c2d1d1ff18294e1ae934877b
amateurdullahan <zabrak12@gmail.com>
2020-07-14 19:41:24 +0000

    ASDADAsdgsd

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
 -- this is a file
 -- script time
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 DROP USER IF EXISTS 'user_0d_2'@'localhost';
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
-FLUSH PRIVLEGES;
+FLUSH PRIVILEGES;
