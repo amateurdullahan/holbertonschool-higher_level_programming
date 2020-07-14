1e441a380da54473355b229c30eeb1286886dae8
amateurdullahan <zabrak12@gmail.com>
2020-07-14 19:42:19 +0000

    trying the dump

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,6 @@
 -- firsto commito
 -- swang song
+DUMP USER IF EXISTS 'user_0d_1'@'localhost';
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
