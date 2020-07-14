1e09ff260090e4f2948607f0084a86c8eda7f21c
amateurdullahan <zabrak12@gmail.com>
2020-07-14 19:56:22 +0000

    forgot to finish auto_increment

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
 -- America is now blood and tears
 -- instead of milk and honey
 CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
-CREATE TABLE IF NOT EXISTS 'hbtn_0d_usa'.'states'(id INT AUTO_ NOT NULL UNIQUE PRIMARY KEY, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS 'htbn_0d_usa'.'states'(id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL, UNIQUE(id));
