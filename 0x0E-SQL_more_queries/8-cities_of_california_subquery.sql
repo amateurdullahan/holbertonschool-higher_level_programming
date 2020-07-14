cb074d05d49bfec0da611aba23aad6f3a559cbc9
amateurdullahan <zabrak12@gmail.com>
2020-07-14 19:51:07 +0000

    hrm

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- Looking out for number one
 -- California, here we come
-SELECT 'cities'.id, cities.name FROM 'cities' WHERE cities.state_id == 1;
+SELECT id, name FROM cities WHERE cities.state_id = 1;
