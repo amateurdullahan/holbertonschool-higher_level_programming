cdbda931cd76a44260080f31bf4b5a3399b1f326
amateurdullahan <zabrak12@gmail.com>
2020-07-14 19:48:27 +0000

    california is id 1

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,3 @@
 -- Looking out for number one
 -- California, here we come
-SELECT 'cities'.id, cities.name
-       FROM 'states', 'cities'
-       WHERE cities.state_id == states.id
-       AND states.name = 'California';
+SELECT 'cities'.id, cities.name FROM 'cities' WHERE cities.state_id == 1;
