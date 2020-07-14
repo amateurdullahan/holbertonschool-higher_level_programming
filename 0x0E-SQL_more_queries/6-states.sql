c52866b071d8c1ecabe665248ce9d9ee31728a7d
amateurdullahan <zabrak12@gmail.com>
2020-07-14 19:58:53 +0000

    ASDASD

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,9 @@
 -- America is now blood and tears
 -- instead of milk and honey
 CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
-CREATE TABLE IF NOT EXISTS 'htbn_0d_usa'.'states'(
-       id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-       name VARCHAR(256) NOT NULL, 
-       UNIQUE(id)
+USE 'hbtn_0d_usa';
+CREATE TABLE IF NOT EXISTS 'states'(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL,
+    UNIQUE(id)
 );
