fe6d50bbb58dc5ef25ae87c9a0fab37f3e618fa8
amateurdullahan <zabrak12@gmail.com>
2020-07-14 20:17:22 +0000

    fixing typos

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,10 +1,10 @@
 -- What does Webster say about soul?
 -- All I want is a good home and a wife
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
-CREATE TABLE IF NOT EXISTS 'htbn_0d_usa'.'cities'(
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS htbn_0d_usa.cities(
        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
        state_id INT NOT NULL,
        name VARCHAR(256) NOT NULL,
-       FOREIGN KEY (state_id) REFERENCES 'hbtn_0d_usa'.'states'(id)
+       FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id),
        UNIQUE(id)
 );
