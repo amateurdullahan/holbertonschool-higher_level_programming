6bd654bfe144e768e2135325019f4ade51d81075
amateurdullahan <zabrak12@gmail.com>
2020-07-14 19:53:49 +0000

    moving order around

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,8 +2,9 @@
 -- All I want is a good home and a wife
 CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
 CREATE TABLE IF NOT EXISTS 'htbn_0d_usa'.'cities'(
-       id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
+       id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
        state_id INT NOT NULL,
        name VARCHAR(256) NOT NULL,
        FOREIGN KEY (state_id) REFERENCES 'hbtn_0d_usa'.'states'(id)
+       UNIQUE(id)
 );
