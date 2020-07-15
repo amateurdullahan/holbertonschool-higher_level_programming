81f8092e2214adaae715b08c5f608c6b3be8467c
amateurdullahan <zabrak12@gmail.com>
2020-07-15 18:20:38 +0000

    pld

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,1 +1,6 @@
-
+-- to day is gonna be the day
+-- that theyre gonna throw it back to you
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+USE hbtn_0c_0;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CHANGE name name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode;
--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,2 +1,3 @@
--- empty
+-- not empty
 -- file
+SELECT tvg.name FROM tvshows tv INNER JOIN tv_show_genres tvsg ON tv.id = tvsg.show_id INNER JOIN tv_genres tvg ON tvsg.genre_id = tvg.id WHERE tv.title = 'Dexter' ORDER BY tvg.name ASC;
