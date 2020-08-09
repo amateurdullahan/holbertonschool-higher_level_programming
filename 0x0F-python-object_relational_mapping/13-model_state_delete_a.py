2ddd05c17cf2dfd42ef8a6189e250e52b4b1f0ef
amateurdullahan <zabrak12@gmail.com>
2020-08-09 18:25:47 +0000

    copy 9 and change one line lol

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""comment"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+if __name__ == "__main__":
+    diesel = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(diesel)
+    Session = sessionmaker(bind=diesel)
+    jam = Session()
+    result = jam.query(State).filter(State.name.like('%a%'))
+    for states in result:
+        jam.delete(states)
+    jam.commit()
+    jam.close()
