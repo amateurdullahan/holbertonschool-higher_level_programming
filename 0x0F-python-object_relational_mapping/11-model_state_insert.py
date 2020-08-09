3fdbf0790bcc0e759aecbc77b35fb9b0f00f02f4
amateurdullahan <zabrak12@gmail.com>
2020-08-09 18:02:37 +0000

    executables now

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -8,11 +8,14 @@ from sys import argv
 
 if __name__ == "__main__":
     diesel = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]))
+        argv[1], argv[2], argv[3], pool_pre_ping=True))
     Base.metadata.create_all(diesel)
     Session = sessionmaker(bind=diesel)
+    Session.configure(bind=diesel)
     jam = Session()
     nstate = State(name='Louisiana')
     jam.add(nstate)
     jam.commit
     print(nstate.id)
+    jam.close()
+
