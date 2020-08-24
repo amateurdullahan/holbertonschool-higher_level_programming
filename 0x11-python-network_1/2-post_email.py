210bd1e91201f4275721b3fdb10ac2ac0c5e392d
amateurdullahan <zabrak12@gmail.com>
2020-08-24 20:52:09 +0000

    if name = main

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,10 +4,12 @@ import urllib.request
 import urllib.parse
 import sys
 
-value = {'email': sys.argv[2]}
-data = urllib.parse.urlencode(value)
-data = data.encode('utf-8')
-req = urllib.request.Request(sys.argv[1], data)
-with urllib.request.urlopen(req) as response:
-    html = response.read().decode(encoding='UTF-8')
-    print(html)
+
+if __name__ == "__main__":
+    value = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(value)
+    data = data.encode('utf-8')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        html = response.read().decode(encoding='UTF-8')
+        print(html)
--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,11 +4,12 @@ import urllib.request
 import sys
 
 
-req = urllib.request.Request(sys.argv[1])
-try:
-    urllib.request.urlopen(req)
-    with urllib.request.urlopen(req) as response:
-        html = response.read().decode('utf-8')
-        print(html)
-except urllib.error.HTTPError as err:
-    print("Error code: {}".format(err.code))
+if __name__ == "__main__":
+    req = urllib.request.Request(sys.argv[1])
+    try:
+        urllib.request.urlopen(req)
+        with urllib.request.urlopen(req) as response:
+            html = response.read().decode('utf-8')
+            print(html)
+    except urllib.error.HTTPError as err:
+        print("Error code: {}".format(err.code))
