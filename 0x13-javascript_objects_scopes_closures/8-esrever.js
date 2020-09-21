cbec8255d9c750d232aefce9ac4059daea9706c0
amateurdullahan <zabrak12@gmail.com>
2020-09-21 23:03:25 +0000

    9, 10 I think

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  let nList = [];
+  const nList = [];
   for (let c = list.length - 1; c >= 0; c--) {
     nList.push(list[c]);
   }
   return nList;
-}
+};
