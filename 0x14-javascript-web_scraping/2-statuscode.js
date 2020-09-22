65ad52e76e45cd6c1a7dddd624c5fdc079d8a4c8
amateurdullahan <zabrak12@gmail.com>
2020-09-22 20:13:53 +0000

    0-3

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (err, response, body) {
+  if (err) {
+    console.error('error:', err);
+  }
+  console.log('code:', response.statusCode);
+});
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const request = require('request');
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (
+  err, response, body) {
+  if (err) {
+    console.log(err);
+  }
+  const jsn = JSON.parse(body);
+  console.log(jsn.title);
+}
+);
