aaf0d5f30c94b7b850a7dce945bc527b7238fa10
Dullahan <zabrak12@gmail.com>
2020-09-23 12:22:21 -0500

    pld

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,4 +4,16 @@ request(process.argv[2], function (err, res, body) {
   if (err) {
     console.log(err);
   }
+  const json = JSON.parse(body);
+  const result = {};
+  for (const obj in json) {
+    if (json[obj].completed) {
+      if (result[json[obj].userId]) {
+        result[json[obj].userId] += 1;
+      } else {
+        result[json[obj].userId] = 1;
+      }
+    }
+  }
+  console.log(result);
 });
