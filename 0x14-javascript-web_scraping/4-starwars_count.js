166031b460cde0b475a21807ccf436ceabfdc8be
Dullahan <zabrak12@gmail.com>
2020-09-23 11:52:21 -0500

    semistandard fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,11 +7,11 @@ request(process.argv[2], function (err, res, body) {
   const json = JSON.parse(body);
   let c = 0;
   for (let a = 0; a < json.results.length; a++) {
-      for (let b = 0; b < json.results[a].characters.length; b++) {
-          if (json.results[a].characters[b === 'https://swapi-api.hbtn.io/api/people/18/']) {
-              c += 1;
-          }
+    for (let b = 0; b < json.results[a].characters.length; b++) {
+      if (json.results[a].characters[b === 'https://swapi-api.hbtn.io/api/people/18/']) {
+        c += 1;
       }
+    }
   }
   console.log(c);
 });
--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
 const fs = require('fs');
-request(process.argv[2], function (err, res, body){
+request(process.argv[2], function (err, res, body) {
   if (err) {
     console.log(err);
   }
-  fs.writeFile(process.argv[3], body, function (err){
+  fs.writeFile(process.argv[3], body, function (err) {
     if (err) {
       console.log(err);
     }
