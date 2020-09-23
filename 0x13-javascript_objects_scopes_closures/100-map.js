358543d5a63059e553ef6bef0846fe32a0184ed3
Dullahan <zabrak12@gmail.com>
2020-09-23 11:44:49 -0500

    pld

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const data = require('./100-data').list;
-const newList = list.map((num, index) => num * index);
+const newList = list.map((x, i) => x * i);
 console.log(data);
 console.log(newList);
